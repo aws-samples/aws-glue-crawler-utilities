3312d02105aefa91d76d3e275902ac3db1774a12
Pradeep Patel <prdept@amazon.com>
2021-09-28 08:44:06 -0700

    Adding sample boto script to automate configuration of S3 Events on S3 target and creation of SQS required for event based crawl

---
+++ samples/boto/S3EventSetup.py
@@ -0,0 +1,255 @@
+#!venv/bin/python
+import boto3
+import botocore
+
+#---------Start : READ ME FIRST ----------------------#
+# 1. Purpose of this script is to create the SQS,SNS and enable S3 Bucket notification.
+#     Following are the operations performed by the scripts:
+#      a. Enable s3 bucket notification to trigger 's3:ObjectCreated:' and 's3:ObjectRemoved:' events.
+#      b. Create SNS topic for fan out.
+#      c. Create SQS queue for saving events which will be consumed by Crawler.
+#          SQS Event Queue ARN will be used to create the crawler after running the script.
+# 2. This script does not create the crawler.
+# 3. SNS topic is created to support FAN out of S3 Events. If S3 event is also used by another
+#    purpose, SNS topic created by the script can be used.
+# 1. Creation of bucket is an optional step.
+#    To create a bucket set create_bucket variable to true.
+# 2. Purpose of crawler_name is to easily locate the SQS/SNS.
+#     crawler_name is used to create SQS and SNS with the same name as crawler.
+# 3. 'folder_name' is the target of crawl inside the specified bucket 's3_bucket_name'
+#
+#---------End : READ ME FIRST ------------------------#
+
+
+#--------------------------------#
+# Start : Configurable settings  #
+#--------------------------------#
+
+#Create
+region = 'us-west-2'
+s3_bucket_name = 's3eventtestuswest2'
+folder_name = "test"
+crawler_name = "test33S3Event"
+sns_topic_name = crawler_name
+sqs_queue_name = sns_topic_name
+create_bucket = False
+
+#-------------------------------#
+# End : Configurable settings   #
+#-------------------------------#
+
+# Define aws clients
+dev = boto3.session.Session(profile_name='myprofile')
+boto3.setup_default_session(profile_name='myprofile')
+s3 = boto3.resource('s3', region_name=region)
+sns = boto3.client('sns', region_name=region)
+sqs = boto3.client('sqs', region_name=region)
+client = boto3.client("sts")
+account_id = client.get_caller_identity()["Account"]
+queue_arn = ""
+
+
+def print_error(e):
+    print(e.message + ' RequestId: ' + e.response['ResponseMetadata']['RequestId'])
+
+def create_s3_bucket(bucket_name, client):
+    bucket = client.Bucket(bucket_name)
+    try:
+        if not create_bucket:
+            return True
+        response = bucket.create(
+            ACL='private',
+            CreateBucketConfiguration={
+                'LocationConstraint': region
+            },
+        )
+        return True
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+        if 'BucketAlreadyOwnedByYou' in e.message:  # we own this bucket so continue
+            print('We own the bucket already. Lets continue...')
+            return True
+    return False
+
+def create_s3_bucket_folder(bucket_name, client, directory_name):
+    s3.put_object(Bucket=bucket_name, Key=(directory_name + '/'))
+
+def set_s3_notification_sns(bucket_name, client, topic_arn):
+    bucket_notification = client.BucketNotification(bucket_name)
+    try:
+
+        response = bucket_notification.put(
+            NotificationConfiguration={
+                'TopicConfigurations': [
+                    {
+                        'Id' : crawler_name,
+                        'TopicArn': topic_arn,
+                        'Events': [
+                            's3:ObjectCreated:*',
+                            's3:ObjectRemoved:*',
+
+                        ],
+                        'Filter' :  {'Key': {'FilterRules': [{'Name': 'prefix', 'Value': folder_name}]}}
+                    },
+                ]
+            }
+        )
+        return True
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return False
+
+
+def create_sns_topic(topic_name, client):
+    try:
+        response = client.create_topic(
+            Name=topic_name
+        )
+        return response['TopicArn']
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return None
+
+def set_sns_topic_policy(topic_arn, client, bucket_name):
+    try:
+        response = client.set_topic_attributes(
+            TopicArn=topic_arn,
+            AttributeName='Policy',
+            AttributeValue='''{
+              "Version": "2008-10-17",
+              "Id": "s3-publish-to-sns",
+              "Statement": [{
+                  "Effect": "Allow",
+                  "Principal": { "AWS" : "*" },
+                  "Action": [ "SNS:Publish" ],
+                  "Resource": "%s",
+                  "Condition": {
+                    "StringEquals": {
+                        "AWS:SourceAccount": "%s"
+                      },
+                      "ArnLike": {
+                          "aws:SourceArn": "arn:aws:s3:*:*:%s"
+                      }
+                  }
+              }]
+  }''' % (topic_arn, account_id, bucket_name)
+        )
+        return True
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+
+    return False
+
+
+def subscribe_to_sns_topic(topic_arn, client, protocol, endpoint):
+    try:
+        response = client.subscribe(
+            TopicArn=topic_arn,
+            Protocol=protocol,
+            Endpoint=endpoint
+        )
+        return response['SubscriptionArn']
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return None
+
+
+def create_sqs_queue(queue_name, client):
+    try:
+        response = client.create_queue(
+            QueueName=queue_name,
+        )
+        return response['QueueUrl']
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return None
+
+
+def get_sqs_queue_arn(queue_url, client):
+    try:
+        response = client.get_queue_attributes(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'QueueArn',
+            ]
+        )
+        return response['Attributes']['QueueArn']
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return None
+
+def set_sqs_policy(queue_url, queue_arn, client, topic_arn):
+    try:
+        response = client.set_queue_attributes(
+            QueueUrl=queue_url,
+            Attributes={
+                'Policy': '''{
+                  "Version": "2012-10-17",
+                  "Id": "AllowSNSPublish",
+                  "Statement": [
+                    {
+                      "Sid": "AllowSNSPublish01",
+                      "Effect": "Allow",
+                      "Principal": "*",
+                      "Action": "SQS:SendMessage",
+                      "Resource": "%s",
+                      "Condition": {
+                        "ArnEquals": {
+                          "aws:SourceArn": "%s"
+                        }
+                      }
+                    }
+                  ]
+}''' % (queue_arn, topic_arn)
+            }
+        )
+        return True
+    except botocore.exceptions.ClientError as e:
+        print_error(e)
+    return False
+
+
+if __name__ == "__main__":
+    print('Creating S3 bucket %s.' % s3_bucket_name)
+    if create_s3_bucket(s3_bucket_name, s3):
+        print('\nCreating SNS topic %s.' % sns_topic_name)
+        topic_arn = create_sns_topic(sns_topic_name, sns)
+        if topic_arn:
+            print('SNS topic created successfully: %s' % topic_arn)
+
+            print('Creating SQS queue %s' % sqs_queue_name)
+            queue_url = create_sqs_queue(sqs_queue_name, sqs)
+            if queue_url is not None:
+                print('Subscribing sqs queue with sns.')
+                queue_arn = get_sqs_queue_arn(queue_url, sqs)
+                if queue_arn is not None:
+                    if set_sqs_policy(queue_url, queue_arn, sqs, topic_arn):
+                        print('Successfully configured queue policy.')
+                        subscription_arn = subscribe_to_sns_topic(topic_arn, sns, 'sqs', queue_arn)
+                        if subscription_arn is not None:
+                            if 'pending confirmation' in subscription_arn:
+                                print('Please confirm SNS subscription by visiting the subscribe URL.')
+                            else:
+                                print('Successfully subscribed SQS queue: ' + queue_arn)
+                        else:
+                            print('Failed to subscribe SNS')
+                    else:
+                        print('Failed to set queue policy.')
+                else:
+                    print("Failed to get queue arn for %s" % queue_url)
+            # ------------ End subscriptions to SNS topic -----------------
+
+            print('\nSetting topic policy to allow s3 bucket %s to publish.' % s3_bucket_name)
+            if set_sns_topic_policy(topic_arn, sns, s3_bucket_name):
+                print('SNS topic policy added successfully.')
+                if set_s3_notification_sns(s3_bucket_name, s3, topic_arn):
+                    print('Successfully configured event for S3 bucket %s' % s3_bucket_name)
+                    print('Create S3 Event Crawler using SQS ARN %s' % queue_arn)
+                else:
+                    print('Failed to configure S3 bucket notification.')
+            else:
+                print('Failed to add SNS topic policy.')
+        else:
+            print('Failed to create SNS topic.')
+
+
